9d39873fd8ab0d927e181efedb6692e5c8fef3b4
wuxifei <wuxifei@finupgroup.com>
2017-12-29 11:15:21 +0800

    add logic of add ip and description

--- vue/app-galaxy/pages/profile/domain/main.vue
+++ vue/app-galaxy/pages/profile/domain/main.vue
@@ -24,71 +24,90 @@
         <!--<div slot="tip" class="el-upload__tip" style="color: #67c23a">只能上传jpg/png文件，且不超过500kb</div>-->
       </el-upload>
     </div>
-    <el-table
-            :data="IPList"
-            border
-            style="width: 100%"
-            v-clickoutside="handleClickOutsideTable"
-    >
-      <el-table-column
-              prop="id"
-              label="编号"
-              width="80">
-      </el-table-column>
-      <el-table-column
-              prop="ip"
-              label="IP白名单"
-              width="180">
-        <template slot-scope="scope">
-          <el-input
-                  class="input-new-tag"
-                  v-if="selected.index == scope.$index && selected.operation == 'modify'"
-                  v-model="selected.row.ip"
-                  size="small"
-                  @keyup.enter.native="handleInputConfirm(scope.$index, scope.row)"
-                  @blur="handleInputConfirm(scope.$index, scope.row)"
-          >
-          </el-input>
-          <span v-else size="small" class="content" @click="">{{scope.row.ip}}</span>
-        </template>
-      </el-table-column>
-      <el-table-column
-              prop="description"
-              label="说明">
-        <template slot-scope="scope">
-          <el-input
-            class="input-new-tag"
-            v-if="selected.index == scope.$index && selected.operation == 'modify'"
-            v-model="selected.row.description"
-            size="small"
-            @keyup.enter.native="handleInputConfirm(scope.$index, scope.row)"
-            @blur="handleInputConfirm(scope.$index, scope.row)"
-          >
-          </el-input>
-          <span v-else size="small" class="content" @click="">{{scope.row.description}}</span>
-        </template>
-      </el-table-column>
-      <el-table-column label="操作" prop="operation" width="160"
-                       headerAlign="center" align="center">
-        <template slot-scope="scope">
+    <div class="manual-area">
+      <el-row class="add-ip">
+        <el-col :span="3">
+          添加白名单
+        </el-col>
+        <el-col :span="6">
+          <el-input placeholder="IP地址" v-model="ip"></el-input>
+        </el-col>
+        <el-col :span="12">
+          <el-input placeholder="说明" v-model="description"></el-input>
+        </el-col>
+        <el-col :span="3">
           <el-button
-            v-if="selected.index == scope.$index && selected.operation == 'modify'"
-            size="mini-extral"
-            type="warning"
-            :loading="waitingResponse"
-            @click="handleRowButtonClick('save', scope.$index, scope.row)">保存</el-button>
-          <el-button
-            size="mini-extral"
-            type="warning"
-            v-else
-            @click="handleRowButtonClick('modify', scope.$index, scope.row)">修改</el-button>
-          <el-button
-            size="mini-extral"
-            type="danger"
-            @click="handleRowButtonClick('delete', scope.$index, scope.row)">删除</el-button>
-        </template>
-      </el-table-column>
-    </el-table>
+              size="mini-extral"
+              type="warning"
+              @click="handleRowButtonClick('add')">保存</el-button>
+        </el-col>
+      </el-row>
+      <el-table
+              :data="IPList"
+              border
+              style="width: 100%"
+              v-clickoutside="handleClickOutsideTable"
+      >
+        <el-table-column
+                type="index"
+                label="编号"
+                width="80">
+        </el-table-column>
+        <el-table-column
+                prop="ip"
+                label="IP白名单"
+                width="180">
+          <template slot-scope="scope">
+            <el-input
+                    class="input-new-tag"
+                    v-if="selected.index == scope.$index && selected.operation == 'modify'"
+                    v-model="selected.row.ip"
+                    size="small"
+                    @keyup.enter.native="handleInputConfirm(scope.$index, scope.row)"
+                    @blur="handleInputConfirm(scope.$index, scope.row)"
+            >
+            </el-input>
+            <span v-else size="small" class="content" @click="">{{scope.row.ip}}</span>
+          </template>
+        </el-table-column>
+        <el-table-column
+                prop="description"
+                label="说明">
+          <template slot-scope="scope">
+            <el-input
+              class="input-new-tag"
+              v-if="selected.index == scope.$index && selected.operation == 'modify'"
+              v-model="selected.row.description"
+              size="small"
+              @keyup.enter.native="handleInputConfirm(scope.$index, scope.row)"
+              @blur="handleInputConfirm(scope.$index, scope.row)"
+            >
+            </el-input>
+            <span v-else size="small" class="content" @click="">{{scope.row.description}}</span>
+          </template>
+        </el-table-column>
+        <el-table-column label="操作" prop="operation" width="160"
+                         headerAlign="center" align="center">
+          <template slot-scope="scope">
+            <el-button
+              v-if="selected.index == scope.$index && selected.operation == 'modify'"
+              size="mini-extral"
+              type="warning"
+              :loading="waitingResponse"
+              @click="handleRowButtonClick('save', scope.$index, scope.row)">保存</el-button>
+            <el-button
+              size="mini-extral"
+              type="warning"
+              v-else
+              @click="handleRowButtonClick('modify', scope.$index, scope.row)">修改</el-button>
+            <el-button
+              size="mini-extral"
+              type="danger"
+              @click="handleRowButtonClick('delete', scope.$index, scope.row)">删除</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+    </div>
   </div>
 </template>
 
@@ -120,16 +139,36 @@
         }
       }
     }
-    .el-table {
-      tr {
-        height: 32px;
+    .manual-area {
+      .add-ip.el-row {
+        width: 80%;
+        margin: 3px auto;
+        line-height: 30px;
+        .el-col {
+          padding: 0px 3px;
+          vertical-align: middle;
+          &:first-child {
+            text-align: right;
+          }
+        }
+        font-size: 14px;
+        .el-input {
+          .el-input__inner {
+            height: 28px;
+          }
+        }
       }
-      td {
-        padding: 0px;
-        .cell {
+      .el-table {
+        tr {
+          height: 32px;
+        }
+        td {
           padding: 0px;
-          .content {
-            padding: 0px 10px;
+          .cell {
+            padding: 0px;
+            .content {
+              padding: 0px 10px;
+            }
           }
         }
       }
@@ -155,16 +194,15 @@
     directives: { Clickoutside },
     data() {
       return {
+        ip: '',
+        description: '',
         IPList: [{
-          id: 1,
           ip: '10.12.34.23',
           description: '上海市普陀区金沙江路 1518 弄',
         }, {
-          id: 2,
           ip: '12.31.233.108',
           description: '上海市普陀区金沙江路 1517 弄',
         }, {
-          id: 3,
           ip: '12.31.233.109',
           description: '上海市普陀区金沙江路 1519 弄',
         }],
@@ -204,6 +242,13 @@
             break;
           case 'delete':
             break;
+          case 'add':
+            this.IPList.unshift({
+              ip: this.ip,
+              description: this.description
+            });
+            this.ip = '';
+            this.description = '';
         }
       },
       handleInputConfirm(index, row) {
