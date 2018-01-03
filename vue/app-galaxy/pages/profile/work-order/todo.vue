73211ce84ef94a08eef8b3d9903639513d16c971
wuxifei <wuxifei@finupgroup.com>
2018-01-03 17:06:56 +0800

    add date range selector in work-order/todo.vue

--- vue/app-galaxy/pages/profile/work-order/todo.vue
+++ vue/app-galaxy/pages/profile/work-order/todo.vue
@@ -1,23 +1,34 @@
 <template>
   <div id="work-order-todo">
-    <el-button
-            size="mini-extral"
-            type="primary"
-            @click="handleButtonClick('linker', {path: '/profile/work-order/todo/add'})">
-      申请工单
-    </el-button>
-    <div class="block">
-      <span class="demonstration">默认</span>
-      <el-date-picker
-              v-model="value3"
-              type="datetimerange"
-              range-separator="至"
-              start-placeholder="开始日期"
-              end-placeholder="结束日期">
-      </el-date-picker>
+    <div class="header">
+      <el-button
+              size="mini-extral"
+              type="primary"
+              @click="handleButtonClick('linker', {path: '/profile/work-order/todo/add'})">
+        申请工单
+      </el-button>
+      <div class="block">
+        <span class="demonstration">申请时间</span>
+        <el-date-picker
+                size="mini"
+                v-model="value3"
+                type="datetimerange"
+                range-separator="至"
+                start-placeholder="开始日期"
+                end-placeholder="结束日期">
+        </el-date-picker>
+      </div>
     </div>
   </div>
 </template>
+<style lang="scss">
+  #work-order-todo {
+    .header {
+      margin: 5px;
+      font-size: 14px;
+    }
+  }
+</style>
 <script>
   export default {
     data() {
