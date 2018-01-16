d46733242665a38010c726cec697343f74eddd60
wuxifei <wuxifei@finupgroup.com>
2018-01-16 11:39:23 +0800

    add component version-selector.vue

--- vue/app-galaxy/pages/profile/log/deploy.vue
+++ vue/app-galaxy/pages/profile/log/deploy.vue
@@ -1,3 +1,17 @@
 <template>
-  <div>this is run/deploy</div>
+  <div id="log-deploy">
+    this is run/deploy
+    <el-version-selector @version-selected="onVersionSelected"></el-version-selector>
+  </div>
 </template>
+<script>
+  import elVersionSelector from '../utils/components/version-selector';
+  export default {
+    components: {elVersionSelector},
+    methods: {
+      onVersionSelected(appId, profileID, version) {
+        console.log(appId, profileID, version);
+      }
+    }
+  }
+</script>
---
+++ vue/app-galaxy/pages/profile/utils/components/version-selector.vue
@@ -0,0 +1,173 @@
+<template>
+  <div class="el-version-selector">
+    <div class="item">
+      <label>应用名称:</label>
+      <el-select v-model="selectedAppID" placeholder="请选择">
+        <el-option v-for="(item, index) in appList" :key="item.appId" :label="item.serviceName" :value="item.appId">
+        </el-option>
+      </el-select>
+    </div>
+    <div class="item">
+      <label>运行环境:</label>
+      <el-select v-model="selectedProfileID" placeholder="请选择">
+        <el-option v-for="item in selectedProfileList" :key="item.id" :label="item.description" :value="item.id">
+        </el-option>
+      </el-select>
+    </div>
+    <div class="item">
+      <label>版本:</label>
+      <el-select v-model="selectedVersion" :placeholder="selectedVersionList.length > 0 ? '加载中' : '当前运行环境下没有版本！'">
+        <el-option v-for="item in selectedVersionList" :key="item" :label="item" :value="item">
+        </el-option>
+      </el-select>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+  .el-version-selector {
+    .el-select .el-input__inner {
+      height: 24px;
+    }
+  }
+</style>
+<style lang="scss" scoped>
+  .el-version-selector {
+    font-size: 14px;
+    .item {
+      display: inline-block;
+    }
+  }
+</style>
+
+<script>
+  import appPropUtils from '../app_prop';
+  import StoreHelper from '../store-helper.vue';
+  export default {
+    mixins: [StoreHelper],
+    created() {
+      this.onAppInfoListOfGroup(this.appInfoListOfGroup);
+    },
+    data() {
+      return {
+        appList: [],
+
+        selectedAppID: null,
+        selectedAPP: null,
+        selectedProfileID: null,
+        selectedProfileList: [],
+        selectedVersion: null,
+        selectedVersionList: [],
+      }
+    },
+    watch: {
+      appInfoListOfGroup: 'onAppInfoListOfGroup',
+      selectedAppID: function (value, oldValue) {
+        let appID = value;
+        let appInfo = this.getAppInfoByID(appID);
+        if (!appInfo) {
+          return;
+        }
+        this.selectedAPP = appInfo['app'];
+        this.selectedProfileList = this.selectedAPP['profileList'];
+        if (Array.isArray(this.selectedProfileList) && this.selectedProfileList.length > 0) {
+          // if value of selectedProfileID is null(at the beginning of this page),
+          // get selectedProfileID from localStorage
+          // else selectedProfileID is the first element in profileList of selectedApp
+          var defaultProfileID = this.selectedProfileList[0]['id'];
+          if (null == this.selectedProfileID) {
+//            let selectedProfileID = this.getConfig('profile/service/profileID');
+//            if (selectedProfileID) {
+//              this.selectedProfileID = selectedProfileID;
+//            }
+            this.selectedProfileID = defaultProfileID;
+          } else {
+            // request service list when app id is changed while profile id is not changed.
+            if (this.selectedProfileID == defaultProfileID) {
+              this.requestVersionList(this.selectedAPP.appId, this.selectedProfileID);
+            } else {
+              this.selectedProfileID = defaultProfileID;
+            }
+          }
+        }
+        this.$setUserConfig('profile/service/appID', appID);
+      },
+      selectedProfileID: function (value, oldValue) {
+        let profileID = value;
+        let appID = this.selectedAPP.appId;
+        this.requestVersionList(appID, profileID);
+//      this.setConfig('profile/service/profileID', profileID);
+      },
+      selectedVersion: function (value, oldValue) {
+        if (null == value) {
+          return;
+        }
+        this.requestInstanceList(this.selectedAPP.appId, this.selectedProfileID, value);
+      },
+    },
+    methods: {
+      /**
+       * call in two place:
+       * 1. created function
+       * 2. appInfoListOfGroup watcher
+       *
+       * what is done?
+       * 1. refresh this.appList
+       * 2. get default appId
+       */
+      onAppInfoListOfGroup(appInfoListOfGroup) {
+        if (appInfoListOfGroup) {
+          if (appInfoListOfGroup.hasOwnProperty('appList')) {
+            this.appList = appInfoListOfGroup.appList;
+          }
+          if (!this.appList || (0 == this.appList.length)) {
+            return;
+          }
+          let appId = this.$getUserConfig('profile/service/appID');
+          if (appId && this.getAppInfoByID(appId)) {
+            this.selectedAppID = appId;
+          } else {
+            this.selectedAppID = this.appList[0]['appId'];
+          }
+        }
+      },
+
+      /**
+       * request version list when selectedAppId or selectedProfileId is changed
+       */
+      requestVersionList(appID, spaceID) {
+        if (!appID || !spaceID) {
+          console.log('appID or spaceID can not be empty');
+          return;
+        }
+        this.selectedVersion = null;
+        this.$net.getServiceVersion({
+          appId: appID,
+          spaceId: spaceID
+        }).then(content => {
+//          console.log(content);
+          if (content.hasOwnProperty('version')) {
+            let version = content.version;
+            if (version && Array.isArray(version) && version.length > 0) {
+              this.selectedVersionList = version;
+              this.selectedVersion = version[0];
+            }
+          }
+        }).catch(err => {
+          console.log(err);
+          this.$message({
+            type: 'error',
+            message: '查找服务版本失败！'
+          });
+        });
+      },
+
+      /**
+       * 获取实例列表
+       */
+      requestInstanceList(appID, spaceID, version) {
+        this.$emit('version-selected', appID, spaceID, version);
+      },
+    }
+  }
+</script>
