e0cd59338466dfdd3fb8fb000dd0bfb3d25fa2b1
wuxifei <wuxifei@finupgroup.com>
2018-02-28 18:26:59 +0800

    sepereate apiPath from path

--- vue/app-galaxy/net/url.js
+++ vue/app-galaxy/net/url.js
@@ -16,164 +16,166 @@ switch (process.env.NODE_ENV) {
     break;
 }
 
-var path = 'http://' + window.location.hostname + ':' + port + '/api';
+var path = 'http://' + window.location.hostname + ':' + port;
+var apiPath = path + '/api';
 
 var urlList = {
   'page_terminal_path': 'http://' + window.location.host + '/terminal.html',
   'page_login_path': 'http://' + window.location.host + '/galaxy.html',
 
   // 获取验证码
-  'get_verify_code': path + '/createRandomImage',
-  'login': path + '/login',
+  'get_verify_code': apiPath + '/createRandomImage',
+  'login': apiPath + '/login',
   // 用户退出
-  'logout': path + '/userLogout',
-  'app_test': path + '/app',
-  'app_list': path + '/application/queryByPage',
-  'user_info': path + '/group/queryByUser',
+  'logout': apiPath + '/userLogout',
+  'app_test': apiPath + '/app',
+  'app_list': apiPath + '/application/queryByPage',
+  'user_info': apiPath + '/group/queryByUser',
   // 获取用户组ID
-  'get_group_id': path + '/group/queryByUser',
-  'create_app': path + '/application/create',
-  'delete_app': path + '/application/delete',
-  'get_profile_of_group': path + '/space/querySpaceByGroupId',
-  'get_cpu_and_memory_config': path + '/cpuAndMemory/queryCpuAndMemory',
-  'get_all_language': path + '/language/queryAllLanguage',
-  'change_profile': path + '/application/update',
+  'get_group_id': apiPath + '/group/queryByUser',
+  'create_app': apiPath + '/application/create',
+  'delete_app': apiPath + '/application/delete',
+  'get_profile_of_group': apiPath + '/space/querySpaceByGroupId',
+  'get_cpu_and_memory_config': apiPath + '/cpuAndMemory/queryCpuAndMemory',
+  'get_all_language': apiPath + '/language/queryAllLanguage',
+  'change_profile': apiPath + '/application/update',
 
   /** 服务相关 */
   // 通过appID和profileID获取服务列表
-  'get_service_by_appId_and_profile': path + '/service/queryByAppIdAndSpaceId',
+  'get_service_by_appId_and_profile': apiPath + '/service/queryByAppIdAndSpaceId',
   // 修改默认服务
-  'change_default_service': path + '/service/switchService',
+  'change_default_service': apiPath + '/service/switchService',
   // 获取服务版本
-  'service_version': path + '/service/queryApplicationServiceVersion',
+  'service_version': apiPath + '/service/queryApplicationServiceVersion',
   // 部署服务-开始部署
-  'service_deploy': path + '/service/deployApplicationService',
+  'service_deploy': apiPath + '/service/deployApplicationService',
   // 部署服务-部署日志
-  'service_deploy_log': path + '/service/deploy/log',
+  'service_deploy_log': apiPath + '/service/deploy/log',
   // 创建服务
-  'service_create':  path + '/service/createApplicationService',
+  'service_create':  apiPath + '/service/createApplicationService',
   // 删除服务
-  'service_delete': path + '/service/deleteApplicationService',
+  'service_delete': apiPath + '/service/deleteApplicationService',
   // 停止服务
-  'service_stop': path + '/service/stopApplicationService',
+  'service_stop': apiPath + '/service/stopApplicationService',
   // 获取自动打镜像类型列表
-  'auto_image_list': path + '/image/queryBasicImage',
+  'auto_image_list': apiPath + '/image/queryBasicImage',
   // 自定义镜像-环境镜像
-  'custom_image_env_list': path + '/image/queryEnvImageByEnvAndGroupTagAndApplicationId',
+  'custom_image_env_list': apiPath + '/image/queryEnvImageByEnvAndGroupTagAndApplicationId',
   // 自定义镜像-私有镜像-项目列表
-  'custom_image_private_app_list': path + '/image/queryProjectNameByGroupTag',
+  'custom_image_private_app_list': apiPath + '/image/queryProjectNameByGroupTag',
   // 自定义镜像-私有镜像-项目-版本列表
-  'custom_image_private_version_list_of_app': path + '/image/queryCustomImage',
+  'custom_image_private_version_list_of_app': apiPath + '/image/queryCustomImage',
 
   // 更改健康检查
-  'service_update_health': path + '/service/updateHealth',
+  'service_update_health': apiPath + '/service/updateHealth',
   // 更改镜像方式
-  'service_update_image': path + '/service/updateImage',
+  'service_update_image': apiPath + '/service/updateImage',
   // 更改gitLabAddress
-  'service_update_gitLab_address': path + '/service/updateGitLabSsh',
+  'service_update_gitLab_address': apiPath + '/service/updateGitLabSsh',
   // 更改gitLabBranch
-  'service_update_gitLab_branch': path + '/service/updateGitLabBranch',
+  'service_update_gitLab_branch': apiPath + '/service/updateGitLabBranch',
   // 更改maven profile id
-  'service_update_maven_profile_id': path + '/service/updateMavenProfileId',
+  'service_update_maven_profile_id': apiPath + '/service/updateMavenProfileId',
   // 更改CPU和Memory
-  'service_update_cpu_and_memory': path + '/service/updateCpuAndMemory',
+  'service_update_cpu_and_memory': apiPath + '/service/updateCpuAndMemory',
   // 更改滚动升级
-  'service_update_rolling_update': path + '/service/updateRollingUpdate',
+  'service_update_rolling_update': apiPath + '/service/updateRollingUpdate',
   // 更改负载均衡
-  'service_update_load_balance': path + '/service/updateLoadBalance',
+  'service_update_load_balance': apiPath + '/service/updateLoadBalance',
   // 更改文件存储
-  'service_update_file_location': path + '/service/updateVolume',
+  'service_update_file_location': apiPath + '/service/updateVolume',
   // 更改环境变量
-  'service_update_environment': path + '/service/updateEnv',
+  'service_update_environment': apiPath + '/service/updateEnv',
   // 更改Host配置
-  'service_update_host': path + '/service/updateHostIp',
+  'service_update_host': apiPath + '/service/updateHostIp',
   // 更改OneAPM
-  'service_update_one_apm': path + '/service/updateOneApm',
+  'service_update_one_apm': apiPath + '/service/updateOneApm',
 
   /** 实例相关*/
   // 获取实例列表
-  'instance_list': path + '/service/queryInstance',
+  'instance_list': apiPath + '/service/queryInstance',
 
   /** 域名相关 */
   // 获取一级域名
-  'domain_level_1_list': path + '/domain/queryBySpaceId',
+  'domain_level_1_list': apiPath + '/domain/queryBySpaceId',
   // 获取当前组的所有一级域名
-  'domain_level_1_list_all': path + '/domain/queryDomainList',
+  'domain_level_1_list_all': apiPath + '/domain/queryDomainList',
   // 添加域名
-  'create_domain': path + '/domain/record/create',
+  'create_domain': apiPath + '/domain/record/create',
   // 删除域名
-  'remove_domain': path + '/domain/record/delete',
+  'remove_domain': apiPath + '/domain/record/delete',
   // 外网域名列表
-  'domain_list': path + '/domain/queryByPage',
+  'domain_list': apiPath + '/domain/queryByPage',
   // 绑定服务
-  'domain_bind_service': path + '/domain/bind',
+  'domain_bind_service': apiPath + '/domain/bind',
   // 解绑服务
-  'domain_unbind_service': path + '/domain/unbind',
+  'domain_unbind_service': apiPath + '/domain/unbind',
   // 添加
-  'domain_add_white_ip': path + '/domain/whiteList/add',
+  'domain_add_white_ip': apiPath + '/domain/whiteList/add',
   // 修改
-  'domain_update_white_ip': path + '/domain/whiteList/',
+  'domain_update_white_ip': apiPath + '/domain/whiteList/',
   // 删除
-  'domain_delete_white_ip': path + '/domain/whiteList/',
+  'domain_delete_white_ip': apiPath + '/domain/whiteList/',
   // 获取白名单列表
-  'domain_white_ip_list': path + '/domain/whiteList/query',
+  'domain_white_ip_list': apiPath + '/domain/whiteList/query',
   // 下载白名单模板
-  'domain_download_white_ip_list_template': path + '/domain/whiteList/download/template',
+  // 'domain_download_white_ip_list_template': apiPath + '/domain/whiteList/download/template',
+  'domain_download_white_ip_list_template': path + '/assets/files/ip白名单模板.xlsx',
   // 上传白名单模板
-  'domain_upload_white_ip_list_template': path + '/domain/whiteList/upload/template',
+  'domain_upload_white_ip_list_template': apiPath + '/domain/whiteList/upload/template',
 
   // 获取部署列表
-  'log_deploy_list': path + '/deployLog/getDeployLog',
+  'log_deploy_list': apiPath + '/deployLog/getDeployLog',
   // 获取运行日志
-  'log_run_log': path + '/searchLog',
+  'log_run_log': apiPath + '/searchLog',
   // 获取部署历史日志
-  'log_deploy_log': path + '/service/historylog',
+  'log_deploy_log': apiPath + '/service/historylog',
 
   // 工单列表
-  'work_order_list': path + '/workOrderDeploy/getWorkOrderDeployList',
-  'work_order_todo_list': path + '/workOrderDeploy/getTodoWorkOrderListByUser',
+  'work_order_list': apiPath + '/workOrderDeploy/getWorkOrderDeployList',
+  'work_order_todo_list': apiPath + '/workOrderDeploy/getTodoWorkOrderListByUser',
   // 工单详情
   // 功能列表
-  'work_order_detail_feature_list': path + '/workOrderDeployFunction/getWorkOrderDeployFunctions',
+  'work_order_detail_feature_list': apiPath + '/workOrderDeployFunction/getWorkOrderDeployFunctions',
   // 程序列表
-  'work_order_detail_app_list': path + '/workOrderDeployApp/getWorkOrderDeployApp',
+  'work_order_detail_app_list': apiPath + '/workOrderDeployApp/getWorkOrderDeployApp',
   // 待办人
-  'work_order_detail_user_todo': path + '/workOrderDeploy/queryWorkOrderDeployToDoUser',
+  'work_order_detail_user_todo': apiPath + '/workOrderDeploy/queryWorkOrderDeployToDoUser',
   // 验收人
-  'work_order_detail_user_accepted': path + '/acceptanceUser/getWorkOrderDeployAcceptUser',
+  'work_order_detail_user_accepted': apiPath + '/acceptanceUser/getWorkOrderDeployAcceptUser',
   // 知会人
-  'work_order_detail_notify_user': path + '/informUser/getWorkOrderDeployInformUser',
+  'work_order_detail_notify_user': apiPath + '/informUser/getWorkOrderDeployInformUser',
   // 操作记录
-  'work_order_detail_operation_list': path + '/workOrderDeployLog/getWorkOrderDeployLog',
+  'work_order_detail_operation_list': apiPath + '/workOrderDeployLog/getWorkOrderDeployLog',
   // 邮件组
-  'work_order_detail_email_group': path + '/emailGroup/getWorkOrderDeployEmailGroup',
+  'work_order_detail_email_group': apiPath + '/emailGroup/getWorkOrderDeployEmailGroup',
 
   /** 处理工单 */
   // 点击处理前的判断逻辑
-  'check_before_handle_work_order': path + '/workOrderDeploy/validWorkOrderDeployHandleUser',
+  'check_before_handle_work_order': apiPath + '/workOrderDeploy/validWorkOrderDeployHandleUser',
   // 上传测试报告
   // 'work_order_handle_upload_test_report': 'http://localhost:4291/post/upload',
-  'work_order_handle_upload_test_report': path + '/workOrderDeploy/uploadFile',
+  'work_order_handle_upload_test_report': apiPath + '/workOrderDeploy/uploadFile',
   // 处理完成
-  'work_order_submit_handle': path + '/workOrderDeploy/handleWorkOrderDeploy',
+  'work_order_submit_handle': apiPath + '/workOrderDeploy/handleWorkOrderDeploy',
   // 创建工单
-  'work_order_create': path + '/workOrderDeploy/applyWordOrderDeploy',
+  'work_order_create': apiPath + '/workOrderDeploy/applyWordOrderDeploy',
   // 删除工单
-  'work_order_remove': path + '/workOrderDeploy/revoked',
+  'work_order_remove': apiPath + '/workOrderDeploy/revoked',
   // 需改工单
-  'work_order_modify': path + '/workOrderDeploy/againSendWorkOrderDeploy',
+  'work_order_modify': apiPath + '/workOrderDeploy/againSendWorkOrderDeploy',
   // 部署应用
-  'work_order_app_deploy': path + '/workOrderDeploy/deploy',
+  'work_order_app_deploy': apiPath + '/workOrderDeploy/deploy',
   // 部署应用-获取日志
-  'work_order_fetch_deploy_log': path + '/workOrderDeploy/searchDeployLog',
+  'work_order_fetch_deploy_log': apiPath + '/workOrderDeploy/searchDeployLog',
 
   // 获取验收人列表
-  'users_in_group': path + '/group/users',
+  'users_in_group': apiPath + '/group/users',
   // 获取所有用户列表
-  'users_all': path + '/user/queryUserList',
+  'users_all': apiPath + '/user/queryUserList',
 
   // 获取打开实例终端信息
-  'terminal_info': path + '/service/queryTerminalInfo',
+  'terminal_info': apiPath + '/service/queryTerminalInfo',
 
 };
 
