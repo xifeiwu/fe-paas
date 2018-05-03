becbd4cba1084fe74fed6384828fcbf0de7a8d29
wuxifei <wuxifei@finupgroup.com>
2018-05-03 16:55:05 +0800

    use custom components for index.html

--- app-galaxy/index.js
+++ app-galaxy/index.js
@@ -1,11 +1,17 @@
 import Vue from 'vue';
 import APP from './index/index.vue';
-// import components from './index/components';
-// components.install(Vue);
+import components from './index/components';
+components.install(Vue);
 
-import(/* webpackChunkName: "components-basic" */ 'assets/libs/components/basic.js').then(components => {
-  components.default.install(Vue);
-  window.vm = new Vue({
-    render: h => h(APP),
-  }).$mount('#app');
-});
+import 'assets/css/fix-style.scss';
+
+window.vm = new Vue({
+  render: h => h(APP),
+}).$mount('#app');
+
+// import(/* webpackChunkName: "components-basic" */ 'assets/libs/components/basic.js').then(components => {
+//   components.default.install(Vue);
+//   window.vm = new Vue({
+//     render: h => h(APP),
+//   }).$mount('#app');
+// });
