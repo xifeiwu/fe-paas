d8f35a1c8620ecfa88857beb348e47f4f251104c
wuxifei <wuxifei@finupgroup.com>
2018-05-10 18:30:57 +0800

    change style of page login, add lib canvas-nest.js

--- app-galaxy/profile/pages/login.vue
+++ app-galaxy/profile/pages/login.vue
@@ -1,178 +1,273 @@
 <template>
-  <el-container id="login">
-    <el-header height="45px">
-      <div class="img" @click="handleHeaderMenuClick(null, ['index'])">
-        <img src="/assets/imgs/finup-cloud.png">
-      </div>
-      <el-menu class="header-menu"
-               mode="horizontal"
-               @select="handleHeaderMenuClick"
-               defaultActive="login">
-        <el-menu-item index="index">凡普云首页</el-menu-item>
-        <el-menu-item index="login">登录</el-menu-item>
-      </el-menu>
-    </el-header>
-    <el-main>
-      <div class="form-container"
-           v-loading="showLoading"
-           element-loading-text="登录中"
-           element-loading-spinner="el-icon-loading"
-           element-loading-background="rgba(255, 255, 255, 0.6)"
+  <div id="login">
+    <img class="finup-cloud" src="/assets/imgs/finup-cloud.png" @click="handleImageClick()">
+    <div class="login-form-container"
+         v-loading="showLoading"
+         element-loading-text="登录中"
+         element-loading-spinner="el-icon-loading"
+         element-loading-background="rgba(255, 255, 255, 0.6)"
+    >
+      <el-form ref="form" :model="form" label-width="0px" class="login-form"
+               @keydown.native="handleKeyDownOnForm"
+               @focus.native.capture="handleFocusOnForm"
       >
-        <el-form ref="form" :model="form" label-width="0px" class="login-form"
-                 @keydown.native="handleKeyDownOnForm"
-                 @focus.native.capture="handleFocusOnForm"
-        >
-          <el-form-item labelWidth="0px">
-            <div class="login-title">登录凡普云</div>
-          </el-form-item>
-          <el-form-item labelWidth="0px" v-if="error.status">
-            <el-alert
-                    :title="error.content"
-                    type="error"
-                    :closable="false"
-                    class="login-error"
-            >
-            </el-alert>
-          </el-form-item>
-          <el-form-item>
-            <el-input v-model="form.userName" placeholder="请输入用户名"></el-input>
-          </el-form-item>
-          <el-form-item>
-            <el-input v-model="form.password" placeholder="请输入密码" type="password"></el-input>
-          </el-form-item>
-          <el-form-item class="verify-code">
-            <el-input v-model="form.verifyCode" placeholder="请输入验证码"
-                      appendStyle="width: 60px; text-align: center"
-            >
-              <template slot="append"><img alt="..." :src="verifyImageData" style="width: 60px; height: 30px; font-size: 10px"
-                                           @click="updateVerifyCode"></template>
-            </el-input>
-          </el-form-item>
-          <el-form-item class="login-interval-item">
-            <el-checkbox v-model="freeLogin15Days"
-                         label="without_login"
-                         name="without_login"
-                         @change="freeLoginStateChange">15天免登录
-            </el-checkbox>
-          </el-form-item>
-          <el-form-item class="login-button-item">
-            <el-button type="primary" class="login-btn" @click="onSubmit">登&nbsp&nbsp&nbsp&nbsp录</el-button>
-          </el-form-item>
-        </el-form>
-      </div>
-    </el-main>
-    <el-footer>
+        <el-form-item labelWidth="0px">
+          <div class="login-title">登录凡普云</div>
+        </el-form-item>
+        <el-form-item labelWidth="0px" v-if="error.status">
+          <el-alert
+                  :title="error.content"
+                  type="error"
+                  :closable="false"
+                  class="login-error"
+          >
+          </el-alert>
+        </el-form-item>
+        <el-form-item>
+          <el-input v-model="form.userName" placeholder="请输入用户名"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-input v-model="form.password" placeholder="请输入密码" type="password"></el-input>
+        </el-form-item>
+        <el-form-item class="verify-code">
+          <el-input v-model="form.verifyCode" placeholder="请输入验证码"
+                    appendStyle="width: 60px; text-align: center"
+          >
+            <template slot="append"><img alt="..." :src="verifyImageData" style="width: 60px; height: 30px; font-size: 10px"
+                                         @click="updateVerifyCode"></template>
+          </el-input>
+        </el-form-item>
+        <el-form-item class="login-interval-item">
+          <el-checkbox v-model="freeLogin15Days"
+                       label="without_login"
+                       name="without_login"
+                       @change="freeLoginStateChange">15天免登录
+          </el-checkbox>
+        </el-form-item>
+        <el-form-item class="login-button-item">
+          <el-button type="primary" class="login-btn" @click="onSubmit">登&nbsp&nbsp&nbsp&nbsp录</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+    <div class="footer">
       <div>Copyright © 2017 凡普金科</div>
-    </el-footer>
-  </el-container>
+    </div>
+    <canvas id="canvas-fe" width="100%" height="100%"></canvas>
+    <canvas id="canvas-bg" width="1950px" height="800px"></canvas>
+  </div>
 </template>
 <style lang="scss" scoped>
-  .el-menu {
-    .el-menu-item {
-      &.is-active {
-        font-weight: bold;
-        cursor: no-drop;
-        font-weight: bold;
-      }
-    }
-  }
   $header-height: 45px;
   $header-background-color: #e7e7e7;
-  #login.el-container {
-    .el-header {
-      background-color: $header-background-color;
-      color: #333;
-      text-align: center;
-      .img {
-        float: left;
-        line-height: $header-height;
-        img {
-          width: 120px;
-          margin-left: 8px;
-        }
-      }
-      .el-menu.header-menu {
-        background-color: transparent;
-        float: right;
-        border-width: 0px;
-        .el-menu-item {
-          font-size: 14px;
-          line-height: $header-height;
-          height: $header-height;
-          &.is-active {
-            color: black;
-          }
-        }
-      }
+  #login {
+    position: fixed;
+    top: 0px;
+    bottom: 0px;
+    left: 0px;
+    right: 0px;
+    img.finup-cloud {
+      z-index: 10;
+      position: absolute;
+      width: 120px;
+      top: 8px;
+      left: 8px;
     }
-    .el-main {
-      position: relative;
-      color: #333;
-      .form-container {
-        float: right;
-        margin-top: 60px;
-        margin-right: 120px;
-        padding: 20px;
-        box-shadow: 0 0 8px 0 rgba(232, 237, 250, .6), 0 2px 4px 0 rgba(232, 237, 250, .5);
-        .el-form.login-form {
-          width: 320px;
-          .el-form-item {
-            margin-bottom: 16px;
-            .login-error {
-              line-height: 100%;
-              padding: 8px 2px;
-              font-weight: bold;
+    .login-form-container {
+      z-index: 10;
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -60%);
+      padding: 20px;
+      box-shadow: 0 0 8px 0 rgba(232, 237, 250, .6), 0 2px 4px 0 rgba(232, 237, 250, .5);
+      .el-form.login-form {
+        width: 320px;
+        .el-form-item {
+          margin-bottom: 16px;
+          .login-error {
+            line-height: 100%;
+            padding: 8px 2px;
+            font-weight: bold;
+          }
+          .login-title {
+            text-align: center;
+            font-size: 1.2rem;
+          }
+          &.login-interval-item {
+            .el-form-item__title {
+              line-height: 24px;
             }
-            .login-title {
+          }
+          &.login-button-item {
+            text-align: center;
+            .el-button {
+              width: 100%;
+            }
+          }
+          &.verify-code {
+            .el-input-group__append {
+              width: 60px;
               text-align: center;
-              font-size: 1.2rem;
             }
-            &.login-interval-item {
-              .el-form-item__title {
-                line-height: 24px;
-              }
-            }
-            &.login-button-item {
-              text-align: center;
-              .el-button {
-                width: 100%;
-              }
-            }
-            &.verify-code {
-              .el-input-group__append {
-                width: 60px;
-                text-align: center;
-              }
-            }
-            &:last-child {
-              margin-bottom: 0px;
-            }
-            .el-form-item__content {
-              /*margin-left: 0px !important;*/
-              input {
-                border-radius: 0px
-              }
+          }
+          &:last-child {
+            margin-bottom: 0px;
+          }
+          .el-form-item__content {
+            /*margin-left: 0px !important;*/
+            input {
+              border-radius: 0px
             }
           }
         }
       }
     }
-    .el-footer {
-      text-align: center;
+    .footer {
+      position: absolute;
+      color: #606266;
+      bottom: 8px;
+      left: 50%;
+      transform: translate(-50%);
     }
-    .el-aside {
-      background-color: #D3DCE6;
-      color: #333;
-      text-align: center;
+    canvas {
+      position: fixed;
+      top: 0;
+      left: 0;
+      bottom: 0px;
+      right: 0px;
+      height: auto;
     }
-  }
-  body > .el-container {
-    margin-bottom: 40px;
+
+    #canvas-fe {
+      z-index: -1;
+    }
+
+    #canvas-bg {
+      z-index: -2;
+      -webkit-filter: blur(3px);
+      -moz-filter: blur(3px);
+      -o-filter: blur(3px);
+      filter: blur(3px);
+      opacity: 0.6;
+    }
   }
 </style>
 
 <script>
+  const drawBG2 = function() {
+    //封装方法，压缩之后减少文件大小
+    function get_attribute(node, attr, default_value) {
+      return node.getAttribute(attr) || default_value;
+    }
+    //封装方法，压缩之后减少文件大小
+    function get_by_tagname(name) {
+      return document.getElementsByTagName(name);
+    }
+    //获取配置参数
+    function get_config_option() {
+      var scripts = get_by_tagname("script"),
+        script_len = scripts.length,
+        script = scripts[script_len - 1]; //当前加载的script
+      return {
+        l: script_len, //长度，用于生成id用
+        z: get_attribute(script, "zIndex", -1), //z-index
+        o: get_attribute(script, "opacity", 0.5), //opacity
+        c: get_attribute(script, "color", "0,0,0"), //color
+        n: get_attribute(script, "count", 99) //count
+      };
+    }
+    //设置canvas的高宽
+    function set_canvas_size() {
+      canvas_width = the_canvas.width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth,
+        canvas_height = the_canvas.height = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+    }
+
+    //绘制过程
+    function draw_canvas() {
+      context.clearRect(0, 0, canvas_width, canvas_height);
+      //随机的线条和当前位置联合数组
+      var e, i, d, x_dist, y_dist, dist; //临时节点
+      //遍历处理每一个点
+      random_points.forEach(function(r, idx) {
+        r.x += r.xa,
+          r.y += r.ya, //移动
+          r.xa *= r.x > canvas_width || r.x < 0 ? -1 : 1,
+          r.ya *= r.y > canvas_height || r.y < 0 ? -1 : 1, //碰到边界，反向反弹
+          context.fillRect(r.x - 0.5, r.y - 0.5, 1, 1); //绘制一个宽高为1的点
+        //从下一个点开始
+        for (i = idx + 1; i < all_array.length; i++) {
+          e = all_array[i];
+          // 当前点存在
+          if (null !== e.x && null !== e.y) {
+            x_dist = r.x - e.x; //x轴距离 l
+            y_dist = r.y - e.y; //y轴距离 n
+            dist = x_dist * x_dist + y_dist * y_dist; //总距离, m
+
+            dist < e.max && (e === current_point && dist >= e.max / 2 && (r.x -= 0.03 * x_dist, r.y -= 0.03 * y_dist), //靠近的时候加速
+              d = (e.max - dist) / e.max,
+              context.beginPath(),
+              context.lineWidth = d / 2,
+              context.strokeStyle = "rgba(" + config.c + "," + (d + 0.2) + ")",
+              context.moveTo(r.x, r.y),
+              context.lineTo(e.x, e.y),
+              context.stroke());
+          }
+        }
+      }), frame_func(draw_canvas);
+    }
+    //创建画布，并添加到body中
+//    var the_canvas = document.createElement("canvas"); //画布
+    var the_canvas = document.getElementById('canvas-fe');
+    var config = get_config_option(), //配置
+      canvas_id = "c_n" + config.l, //canvas id
+      context = the_canvas.getContext("2d"), canvas_width, canvas_height,
+      frame_func = window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame || function(func) {
+          window.setTimeout(func, 1000 / 45);
+        }, random = Math.random,
+      current_point = {
+        x: null, //当前鼠标x
+        y: null, //当前鼠标y
+        max: 20000 // 圈半径的平方
+      },
+      all_array;
+    the_canvas.id = canvas_id;
+    the_canvas.style.cssText = "position:fixed;top:0;left:0;z-index:" + config.z + ";opacity:" + config.o;
+//    get_by_tagname("body")[0].appendChild(the_canvas);
+
+    //初始化画布大小
+    set_canvas_size();
+    window.onresize = set_canvas_size;
+    //当时鼠标位置存储，离开的时候，释放当前位置信息
+    window.onmousemove = function(e) {
+      e = e || window.event;
+      current_point.x = e.clientX;
+      current_point.y = e.clientY;
+    }, window.onmouseout = function() {
+      current_point.x = null;
+      current_point.y = null;
+    };
+    //随机生成config.n条线位置信息
+    for (var random_points = [], i = 0; config.n > i; i++) {
+      var x = random() * canvas_width, //随机位置
+        y = random() * canvas_height,
+        xa = 1 * random() - 1, //随机运动方向
+        ya = 1 * random() - 1;
+      // 随机点
+      random_points.push({
+        x: x,
+        y: y,
+        xa: xa,
+        ya: ya,
+        max: 6000 //沾附距离
+      });
+    }
+    all_array = random_points.concat([current_point]);
+    //0.1秒后绘制
+    setTimeout(function() {
+      draw_canvas();
+    }, 100);
+  };
+
   const keyCode = Object.freeze({
     LEFT: 37,
     UP: 38,
@@ -212,19 +307,11 @@
       setTimeout(() => {
         this.focusableElesInForm.length > 0 && this.focusableElesInForm[0].focus();
       }, 1000);
+      drawBG2();
     },
     methods: {
-      handleHeaderMenuClick(key, keyPath) {
-        keyPath = keyPath.join('/');
-        switch (keyPath) {
-          case 'profile':
-            this.$router.push('/profile');
-            break;
-          case 'index':
-//            this.$router.push('/index');
-            this.$utils.goToPath('/index');
-            break;
-        }
+      handleImageClick() {
+        this.$utils.goToPath('/index');
       },
       handleKeyDownOnForm(evt) {
         switch (evt.keyCode) {
