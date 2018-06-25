efdc92445976f8370456c88ab22abcf367e56a35
wuxifei <wuxifei@finupgroup.com>
2018-06-25 17:44:25 +0800

    disable close button for time-picker of page log/run

--- app-galaxy/profile/pages/profile/log/run.vue
+++ app-galaxy/profile/pages/profile/log/run.vue
@@ -27,7 +27,9 @@
                 range-separator="至"
                 start-placeholder="开始日期"
                 end-placeholder="结束日期"
-                align="right">
+                align="right"
+                :enableClose="false"
+        >
         </el-date-picker>
       </div>
       <div class="item">
