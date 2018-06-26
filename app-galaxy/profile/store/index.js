7ef1acc3c0ce412419614f224f22a42111cec300
高乐天 <gaoletian@puhuifinance.com>
2018-06-26 16:29:45 +0800

    feat use vuex-persistedstate

--- app-galaxy/profile/store/index.js
+++ app-galaxy/profile/store/index.js
@@ -4,6 +4,7 @@
 
 import Vue from 'vue';
 import Vuex from 'vuex';
+import createPersistedState from 'vuex-persistedstate';
 import user from './modules/user';
 import app from './modules/app';
 import tmp from './modules/tmp';
@@ -15,5 +16,10 @@ Vue.use(Vuex);
 export default new Vuex.Store({
   modules:{
     user, app, tmp, global, etc
-  }
+  },
+  plugins: [createPersistedState({
+    key: 'galaxy',
+    // 暂时只持久化 etc 模块，防止冲突
+    paths: ['etc']
+  })]
 })
