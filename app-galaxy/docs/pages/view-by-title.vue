18844564ac5263dcb6cc194fa42c81f24e4c4834
wuxifei <wuxifei@finupgroup.com>
2018-08-31 14:57:20 +0800

    add page view-by-title

---
+++ app-galaxy/docs/pages/view-by-title.vue
@@ -0,0 +1,255 @@
+<template>
+  <div id="docs-paas">
+    <div class="section-navigation">
+      <el-scrollbar>
+        <el-tree ref="menu-list"
+                 :data="menuList"
+                 :props="defaultProps"
+                 nodeKey='target'
+                 :setCurrentNodeOnClick="false"
+                 :expandOnClickNode="false"
+                 @node-click="handleNodeClick">
+        </el-tree>
+        <div class="navigation markdown-body" style="display: none" ref="navigation"></div>
+      </el-scrollbar>
+    </div>
+    <div class="section-content">
+      <el-scrollbar>
+        <div v-html="netData.html" class="content markdown-body">
+        </div>
+      </el-scrollbar>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+  #docs-paas {
+    .el-tree {
+      font-weight: bold;
+      .el-tree-node__label {
+        font-size: 16px;
+        line-height: 1.5;
+      }
+      .el-tree-node {
+        .el-tree-node__children {
+          .el-tree-node {
+            &.is-current {
+              & > .el-tree-node__content {
+                border-radius: 4px;
+                background-color: #409EFF;
+                color: white;
+              }
+            }
+            .el-tree-node__content {
+              span.is-leaf + span {
+                font-weight: normal;
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+</style>
+<style lang="scss">
+  #docs-paas {
+    position: relative;
+    height: 100%;
+    padding-top: 5px;
+    box-sizing: border-box;
+    .section-navigation {
+      width: 320px;
+      position: absolute;
+      left: 0px;
+      top: 10px;
+      bottom: 15px;
+      z-index: 10;
+      margin: 0;
+      box-sizing: border-box;
+      box-shadow: 0 2px 6px 0 rgba(0,0,0,.04);
+      border-radius: 9px;
+      background-color: white;
+      .el-scrollbar {
+        margin-top: 5px;
+        height: 100%;
+        width: 100%;
+      }
+    }
+    .section-content {
+      margin-left: 320px;
+      height: 100%;
+      padding-left: 10px;
+      padding-right: 5px;
+      .el-scrollbar {
+        height: 100%;
+        width: 100%;
+      }
+      .markdown-body {
+      }
+    }
+  }
+</style>
+<script>
+
+  export default {
+    created() {
+      if (this.$route.params.hasOwnProperty('title')) {
+        this.title = this.$route.params['title'];
+      } else {
+
+      }
+
+      this.$net.requestAssistServer(this.$net.URL_LIST.md_detail, {
+        payload: {title: '云平台帮助文档'}
+      }).then(resContent => {
+        this.netData.html = resContent['html'];
+        this.netData.markdown = resContent['markdown'];
+      }).catch();
+    },
+    mounted() {
+      this.contentScrollWrapper = this.$el.querySelector('.section-content .el-scrollbar .el-scrollbar__wrap');
+      this.contentNode = this.contentScrollWrapper.querySelector('.content.markdown-body');
+    },
+    data() {
+      return {
+        title: null,
+        contentScrollWrapper: null,
+        menuList: [{
+          label: ''
+        }],
+        netData: {
+          markdown: '',
+          html: ''
+        },
+        defaultProps: {
+          children: 'children',
+          label: 'label',
+          isLeaf: 'isLeaf',
+        }
+      };
+    },
+    watch: {
+      '$route': function (value) {
+        console.log(value);
+      },
+      'netData.html': function (value) {
+        if (!this.$refs.hasOwnProperty('navigation')) {
+          return;
+        }
+        const nodeList = [];
+//        const navigation = this.$refs['navigation'];
+        const  navigationNode = document.createElement('div');
+        navigationNode.innerHTML = this.netData.html;
+        Array.prototype.slice.call(navigationNode.children).forEach((node, index) => {
+//          let reg = /^H[1-8]{1}$/;
+          const reg =/^<h([1-8])(?: id="(.*)")*>(.*)<\/h\1>$/;
+          const match = reg.exec(node.outerHTML);
+          if (!match) {
+            node.style.display = 'none'
+          } else {
+            // get level, target, and text by regexp
+            node.index = index;
+            if (match.length === 4) {
+              node.level = match[1];
+              node.target = `#${match[2]}`;
+              node.label = match[3].trim();
+            } else if (match.length === 3) {
+              node.level = match[1];
+              node.target = `#${match[2]}`;
+              node.label = match[2].trim();
+            }
+            nodeList.push(node);
+//            node.addEventListener('click', (evt) => {
+//              let target = evt.target;
+//              while (!reg.exec(target.outerHTML)) {
+//                target = target.parentNode;
+//              }
+//              this.contentScrollWrapper.scrollTop = this.contentNode.children[index].offsetTop;
+//            });
+          }
+        });
+
+        let levelStack = [];
+        let levelList = [];
+        const pushIt = function(list, item) {
+          if (!list.hasOwnProperty('children')) {
+            list.children = [];
+          }
+          list.children.push(item);
+        };
+        nodeList.map(node => {
+          return {
+            index: node.index,
+            level: node.level,
+            target: node.target,
+            label: node.label
+          }
+        }).forEach(it => {
+//          console.log(it);
+//          return;
+          let lastItem = null;
+          if (levelStack.length === 0) {
+            levelStack.push(it);
+            levelList.push(it);
+          } else {
+            lastItem = levelStack[levelStack.length - 1];
+            // console.log(lastItem.level, lastItem.word);
+            if (lastItem.level < it.level) {
+              pushIt(lastItem, it);
+              levelStack.push(it);
+            } else {
+              levelStack.pop();
+              // console.log(levelStack);
+              if (levelStack.length === 0) {
+                levelStack.push(it);
+                levelList.push(it);
+              } else {
+                lastItem = levelStack[levelStack.length - 1];
+                // console.log(lastItem);
+                // console.log(it);
+                while (lastItem && lastItem.level >= it.level && levelStack.length > 0) {
+                  levelStack.pop();
+                  if (levelStack.length === 0) {
+                    lastItem = null;
+                  } else {
+                    lastItem = levelStack[levelStack.length - 1];
+                  }
+                }
+                if (lastItem) {
+                  pushIt(lastItem, it);
+                  levelStack.push(it);
+                } else {
+                  levelStack.push(it);
+                  levelList.push(it);
+                }
+              }
+            }
+          }
+        });
+//        console.log(levelStack);
+//        console.log(levelList);
+        this.menuList = levelList;
+      }
+    },
+    methods: {
+      updateScrollTopByHash() {
+        let hash = location.hash;
+        if (!hash) {
+          return;
+        }
+        let target = this.$el.querySelector(decodeURI(hash));
+        if (target) {
+          this.contentScrollWrapper.scrollTop = target.offsetTop;
+        }
+      },
+      handleNodeClick(data) {
+        if (data.hasOwnProperty('target')) {
+          let target = data['target'];
+          window.location.hash = target;
+          this.updateScrollTopByHash();
+          this.$refs['menu-list'].setCurrentKey(target);
+        }
+      }
+    }
+  };
+</script>
