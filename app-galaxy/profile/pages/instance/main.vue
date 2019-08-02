4d4addc2f2a5051fc350e1aa92f9c7ff4c71837d
wuxifei <wuxifei@finupgroup.com>
2019-08-02 16:26:57 +0800

    not check before open new terminal

--- app-galaxy/profile/pages/instance/main.vue
+++ app-galaxy/profile/pages/instance/main.vue
@@ -892,13 +892,13 @@
             }
             break;
           case 'go-to-page-instance-terminal-from-instance':
-            if(row.status != '运行中'){
-              this.$storeHelper.globalPopover.show({
-                ref: evt.target,
-                msg: '非运行中的实例不能打开终端'
-              });
-              return;
-            }
+//            if(row.status != '运行中'){
+//              this.$storeHelper.globalPopover.show({
+//                ref: evt.target,
+//                msg: '非运行中的实例不能打开终端'
+//              });
+//              return;
+//            }
             infoForPageTerminal = this.getInfoForPageInstanceTerminal(row);
             if (!infoForPageTerminal) {
               this.$message.error('所需信息不完整，请刷新页面重试！');
