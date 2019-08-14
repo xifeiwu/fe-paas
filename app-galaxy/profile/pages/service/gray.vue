f154c09e0154b912235ae3e2060e526668c697a3
wuxifei <wuxifei@finupgroup.com>
2019-08-14 15:02:33 +0800

    finish service list in page service/gray

--- app-galaxy/profile/pages/service/gray.vue
+++ app-galaxy/profile/pages/service/gray.vue
@@ -1,6 +1,62 @@
 <template>
-  <div id="service-gray"></div>
+  <div id="service-gray">
+    <div class="header">
+      <el-button size="mini" type="primary" style="margin-right: 5px" @click="handleClick($event, 'service_gray_create')">
+        <span>创建灰度版本</span>
+      </el-button>
+      <el-button size="mini" type="primary" style="margin-right: 5px" @click="handleClick($event, 'service_gray_strategy')">
+        <span>设置灰度策略</span>
+      </el-button>
+      <el-button size="mini" type="primary" style="margin-right: 5px" @click="handleClick($event, 'refresh')">
+        <span>刷新</span>
+        <i class="el-icon el-icon-refresh" style="margin-left: 3px;"></i>
+      </el-button>
+    </div>
+    <div class="list">
+      <el-table :data="serviceList"
+                stripe>
+        <el-table-column label="服务版本" prop="serviceTypeName"></el-table-column>
+        <el-table-column label="运行实例数/总实例数" prop="instanceStatus" headerAlign="center" align="center" width="200">
+        </el-table-column>
+        <el-table-column>
+          <template slot-scope="scope">
+
+          </template>
+        </el-table-column>
+      </el-table>
+    </div>
+  </div>
 </template>
+<style lang="scss" scoped>
+  #service-gray {
+    background-color: white;
+    height:100%;
+    max-width: 1500px;
+    display: flex;
+    flex-direction: column;
+    & > .header {
+      padding: 3px 5px;
+      font-size: 14px;
+      min-height: 28px;
+      .item {
+        display: inline-block;
+        margin-right: 5px;
+      }
+      .el-select {
+        width: 180px;
+      }
+      .toggle-warning {
+        display: inline-block;
+        line-height: 24px;
+        margin-left: 12px;
+        color: #eb9e05;
+      }
+    }
+    & > .list {
+      flex: 1;
+    }
+  }
+</style>
 <script>
   export default {
     async created() {
@@ -19,18 +75,64 @@
         }
       });
       console.log(resContent);
-      resContent = await this.$net.requestPaasServer(this.$net.URL_LIST.service_gray_list, {
-        query: {
-          configId: this.serviceId
-        }
-      });
-      console.log(resContent);
+      this.requestServiceList();
     },
     async mounted() {
+      this.onScreenSizeChange(this.$storeHelper.screen.size);
     },
     data() {
       return {
-        serviceId: null
+        serviceId: null,
+        serviceList: [],
+      }
+    },
+    methods: {
+      onScreenSizeChange(size) {
+        if (!size) {
+          size = this.$storeHelper.screen.size;
+        }
+        try {
+          const headerNode = this.$el.querySelector(':scope > .header');
+          const headerHeight = headerNode.offsetHeight;
+          this.heightOfTable = this.$el.clientHeight - headerHeight;
+        } catch(err) {
+        }
+      },
+      async requestServiceList() {
+        this.serviceList = [];
+        const resContent = await this.$net.requestPaasServer(this.$net.URL_LIST.service_gray_list, {
+          query: {
+            configId: this.serviceId
+          }
+        });
+        const postTreat = it => {
+          if (it.hasOwnProperty('containerStatus') && this.$utils.hasProps(it['containerStatus'], 'Running', 'Total')) {
+            it.instanceStatus = `${it['containerStatus']['Running']}/${it['containerStatus']['Total']}`
+          } else {
+            it.instanceStatus = '---/---';
+          }
+          it.formattedCreateTime = this.$utils.formatDate(it.createTime, 'yyyy-MM-dd hh:mm:ss');
+          return it;
+        };
+
+        // console.log(resContent);
+        if (resContent['master']) {
+          resContent['master']['serviceType'] = 'master';
+          resContent['master']['serviceTypeName'] = '主服务';
+          this.serviceList.push(postTreat(resContent['master']));
+        }
+        console.log(this.serviceList);
+      },
+      handleClick(evt, action, data) {
+        switch (action) {
+          case 'service_gray_create':
+            break;
+          case 'service_gray_strategy':
+            break;
+          case 'refresh':
+            this.requestServiceList();
+            break;
+        }
       }
     }
   }
