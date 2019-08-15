6ba0adaa47fd39b79d295b2eba53c76261a56340
wuxifei <wuxifei@finupgroup.com>
2019-08-15 17:18:05 +0800

    fix style for healthCheck

--- app-galaxy/profile/pages/service/add-v2.vue
+++ app-galaxy/profile/pages/service/add-v2.vue
@@ -160,32 +160,26 @@
                   </div>
                 </div>
               </el-form-item>
-              <el-form-item>
-                <div class="initial-delay" style="line-height: 28px">
+              <el-form-item class="health-check-more">
+                <div class="initial-delay item" style="line-height: 28px">
                   <span>延迟时间：</span>
                   <el-input-number v-model="formData.healthCheck.initialDelay" :min="30" :max="300" label="延迟时间"></el-input-number>
                   <i class="paas-icon-fa-question" v-pop-on-mouse-over="'单位秒。指实例启动后，多久开始探测。例如，启动延时设置为5，那么健康检查将在实例启动5秒后开始。默认值为120，范围30-300'"></i>
                 </div>
-              </el-form-item>
-              <el-form-item>
-                <div class="period-seconds" style="line-height: 28px">
-                    <span>间隔时间：</span>
-                    <el-input-number v-model="formData.healthCheck.periodSeconds" :min="1" :max="10" label="间隔时间"></el-input-number>
-                    <i class="paas-icon-fa-question" v-pop-on-mouse-over="'单位秒。指健康检查的频率。例如，间隔时间设置成10，那么集群会每隔10s检查一次。默认值为5，范围1-10'"></i>
+                <div class="period-seconds item" style="line-height: 28px">
+                  <span>间隔时间：</span>
+                  <el-input-number v-model="formData.healthCheck.periodSeconds" :min="1" :max="10" label="间隔时间"></el-input-number>
+                  <i class="paas-icon-fa-question" v-pop-on-mouse-over="'单位秒。指健康检查的频率。例如，间隔时间设置成10，那么集群会每隔10s检查一次。默认值为5，范围1-10'"></i>
                 </div>
-              </el-form-item>
-              <el-form-item>
-                <div class="failure-threshold" style="line-height: 28px">
-                    <span>不健康阈值：</span>
-                    <el-input-number v-model="formData.healthCheck.failureThreshold" :min="1" :max="10" label="不健康阈值"></el-input-number>
-                    <i class="paas-icon-fa-question" v-pop-on-mouse-over="'单位次。该参数指健康检查连续失败多少次后，才判定实例是不健康的。例如不健康阈值设置成 3，只有满足连续 3 次都探测失败了，才认为容器是不健康的。默认值为5，范围1-10'"></i>
+                <div class="failure-threshold item" style="line-height: 28px">
+                  <span>不健康阈值：</span>
+                  <el-input-number v-model="formData.healthCheck.failureThreshold" :min="1" :max="10" label="不健康阈值"></el-input-number>
+                  <i class="paas-icon-fa-question" v-pop-on-mouse-over="'单位次。该参数指健康检查连续失败多少次后，才判定实例是不健康的。例如不健康阈值设置成 3，只有满足连续 3 次都探测失败了，才认为容器是不健康的。默认值为5，范围1-10'"></i>
                 </div>
-              </el-form-item>
-              <el-form-item>
-                <div class="timeout-seconds" style="line-height: 28px">
-                    <span>响应超时：</span>
-                    <el-input-number v-model="formData.healthCheck.timeoutSeconds" :min="10" :max="120" label="响应超时"></el-input-number>
-                    <i class="paas-icon-fa-question" v-pop-on-mouse-over="'单位秒。指健康探测的超时时间。在这里是HTTP 请求响应超时时间。默认值为10，范围10-120'"></i>
+                <div class="timeout-seconds item" style="line-height: 28px">
+                  <span>响应超时：</span>
+                  <el-input-number v-model="formData.healthCheck.timeoutSeconds" :min="10" :max="120" label="响应超时"></el-input-number>
+                  <i class="paas-icon-fa-question" v-pop-on-mouse-over="'单位秒。指健康探测的超时时间。在这里是HTTP 请求响应超时时间。默认值为10，范围10-120'"></i>
                 </div>
               </el-form-item>
             </div>
@@ -388,6 +382,15 @@
       .el-form-item {
         .el-form-item__label {
           font-weight: bold;
+        }
+        &.health-check-more {
+          .el-form-item__content {
+            display: block;
+            .item {
+              width: 300px;
+              display: inline-block;
+            }
+          }
         }
         &.service-version {
           max-width: 500px;
@@ -657,18 +660,6 @@
           // NOTICE: the sequence of initialize should not change
           this.formRelated.languageInfo = serviceInfo.language;
           this.formData.appId = serviceInfo.appId;
-
-          if (this.$storeHelper.groupVersion == 'v1' && this.formRelated.languageInfo.type.toUpperCase() === 'PYTHON') {
-            this.imageSelectState.customImage = true;
-          } else {
-            this.imageSelectState.customImage = serviceInfo.image.customImage;
-          }
-          // imageLoaction should set in function requestImageRelatedInfo
-          if (serviceInfo.customImage) {
-            // set after requestImageRelatedInfo
-          } else {
-            // set after requestImageRelatedInfo
-          }
           this.formData.gitLabAddress = serviceInfo.gitLabAddress;
           this.formData.gitLabBranch = serviceInfo.gitLabBranch;
           this.formData.appMonitor = serviceInfo.appMonitor;
@@ -693,6 +684,17 @@
           this.$nextTick(() => {
             this.formData.healthCheck = serviceInfo.healthCheck;
           });
+          if (this.$storeHelper.groupVersion == 'v1' && this.formRelated.languageInfo.type.toUpperCase() === 'PYTHON') {
+            this.imageSelectState.customImage = true;
+          } else {
+            this.imageSelectState.customImage = serviceInfo.image.customImage;
+          }
+          // imageLoaction should set in function requestImageRelatedInfo
+          if (serviceInfo.customImage) {
+            // set after requestImageRelatedInfo
+          } else {
+            // set after requestImageRelatedInfo
+          }
           if (serviceInfo.portMap.outerPort && serviceInfo.portMap.outerPort !== ""
             && serviceInfo.portMap.action && serviceInfo.portMap.action !== 'del') {
             this.formData.portMap.outerPort = serviceInfo.portMap.outerPort;
