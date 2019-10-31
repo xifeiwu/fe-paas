c62690683232829e11d35edbce80cf9beec65cb7
liugang <liugang@finupgroup.com>
2019-10-31 16:43:06 +0800

    修改灰度提示信息

--- app-galaxy/profile/pages/service/gray/list.vue
+++ app-galaxy/profile/pages/service/gray/list.vue
@@ -775,7 +775,7 @@
         const target = evt.target;
 
         if (action == 'service_gray_create' && this.canaryStatus && !this.canaryStatus['hasVersionLabel']) {
-          await this.$alert('如需使用灰度发布，请先重启或重新部署该服务', '提示');
+          await this.$alert('首次使用灰度发布，请先重启或重新部署该服务', '提示');
           return;
         }
 
